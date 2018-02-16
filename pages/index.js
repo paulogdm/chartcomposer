0aa110af44ce5b618c529e4c98f978d19dcaab4c
Lindsey Simon <lindsey@quip.com>
2018-02-16 09:00:18 -0800

    basic?

--- pages/index.js
+++ pages/index.js
@@ -1,1 +1,13 @@
-export default () => <div>Welcome to next.js!</div>
+
+import chordpro from 'chordprojs'
+import sleepy from '../songz/SleepyAngel.txt'
+
+export default () => {
+    const html = chordpro.format(sleepy)
+    return (
+        <div>
+            Welcome to next.js / muzik!<br/>
+            {html}
+        </div>
+    )
+}
