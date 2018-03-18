b8fc2982976c1039e830d5b3303b509bbdb73a08
Steve Souders <steve@speedcurve.com>
2018-03-18 14:08:38 -0700

    arrange columns equally - move button to textarea column

--- components/SongEditor.jsx
+++ components/SongEditor.jsx
@@ -6,40 +6,41 @@ const SongEditor = ({ value, onChange, onSave }) => {
   }
   return (
     <div style={{ position: "relative", height: "100%" }}>
-      <button
-        onClick={onSave}
-        style={{
-          position: "absolute",
-          top: 0,
-          right: 0,
-        }}
-      >
-        Save
-      </button>
       <div
         style={{
           display: "flex",
           height: "100%",
         }}
       >
-        <textarea
-          value={value}
-          onChange={onChange}
-          style={{
-            border: "none",
-            width: "100%",
-            height: "100%",
-            padding: 0,
-          }}
-        />
-        <SongView value={value} />
+		<div style={{width: "40%"}}>
+	      <button
+			 onClick={onSave}
+			 style={{
+				     background: "#525",
+				     color: "#FFF"
+			       }}
+			 >
+			Save
+		  </button>
+          <textarea
+			 value={value}
+			 onChange={onChange}
+			 style={{
+                     border: "none",
+                     width: "100%",
+                     height: "100%",
+                     padding: 0,
+			 }}
+			 />
+		</div>
+		<SongView value={value} />
       </div>
     </div>
   );
 };
 
 const SongView = ({ value }) => {
-  return <div dangerouslySetInnerHTML={chordProParse(value)} />;
+  return <div style={{width: "60%"}} dangerouslySetInnerHTML={chordProParse(value)} />;
 };
 
 export default SongEditor;
