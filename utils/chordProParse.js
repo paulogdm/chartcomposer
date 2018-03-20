8075323cf3b8185a9444f9849aa795eba6a7947e
Steve Souders <steve@speedcurve.com>
2018-03-19 21:17:27 -0700

    fix styles

--- utils/chordProParse.js
+++ utils/chordProParse.js
@@ -1,7 +1,7 @@
 export default function chordProParse(value) {
 	gSong = importChordPro(value);
 	var sHtml = exportHtml(gSong);
-	return { __html: sHtml }
+	return { __html: "<div class=outersong style='margin: 0.5em; font-family: Verdana, Arial, Helvetica, sans-serif;'>" + sHtml + "</div>" }
 }
 
 const greeting = "hey ho: ";
@@ -84,6 +84,12 @@ function doBlock(type, closingdirectives) {
         else if ( "" === line && 0 === aClosingDirectives.length ) {
             console.log("debug: assuming this blank line closes the current \"" + type + "\" block");
             break;
+        }
+        else if ( isDirective(line) ) {
+            doDirective(line);
+        }
+        else if ( isComment(line) ) {
+            // do nothing
         }
         else {
             block.lines.push(line);
@@ -296,12 +302,12 @@ function doDirective(line) {
 function exportHtml(song) {
     var aResults = [];
 
-    aResults.push("<div class=songproperties>");
+    aResults.push("<div class=songproperties style='margin-bottom: 1em;'>");
     var aProperties = [ "title", "subtitle", "composer", "artist", "key", "tempo", "capo" ];
     for ( var i = 0; i < aProperties.length; i++ ) {
         var prop = aProperties[i];
         if ( song[prop] ) {
-            aResults.push("<div class=song" + prop + ">" + ( "title" === prop || "subtitle" === prop ? "" : prop + ": " ) + song[prop] + "</div>");
+            aResults.push("<div class=song" + prop + getCss(prop) + ">" + ( "title" === prop || "subtitle" === prop ? "" : prop + ": " ) + song[prop] + "</div>");
         }
     }
     aResults.push("</div>");
@@ -316,6 +322,29 @@ function exportHtml(song) {
 }
 
 
+// Need this cause CSS rules are missing
+function getCss(prop) {
+	switch (prop) {
+	case 'title':
+		return " style='font-size: 1.5em; font-weight: bold;'";
+	case 'artist':
+		return " style='font-size: 1.1em;'";
+	case 'composer':
+		return " style='font-size: 1.1em;'";
+	case 'verse':
+	case 'chorus':
+	case 'tab':
+		return " style='padding-top: 0.5em; margin-bottom: 0.5em; padding-left: 0.5em;'";
+	case 'comment':
+		return " style='padding-top: 0.5em; padding-bottom: 0.5em;'";
+	case 'chorus':
+		return " style='border-left: 5px solid #CCC;'";
+	}
+
+	return "";
+}
+
+
 function exportHtmlPart(aParts, i) {
     var aResults = [];
     var part = aParts[i];
@@ -327,14 +356,14 @@ function exportHtmlPart(aParts, i) {
         sStyle = " style='margin-bottom: -0.5em; padding-top: 0.5em;'";
     }
 
-    aResults.push("<div class=song" + part.type + sStyle + ">");
+    aResults.push("<div class=song" + part.type + sStyle + getCss(part.type) + ">");
 
     if ( part.lines ) {
         for ( var i = 0; i < part.lines.length; i++ ) {
             var line = part.lines[i];
-            line = line.replace(/\[/g, "<span class='chord settingschord'>").replace(/\]/g, "</span>");
-            line = ( "comment" === part.type || "choruscomment" === part.type ? "<div class=lyriccomment>" + line + "</div><div style='clear: both;'></div>" : line );
-            aResults.push("<div class=lyricline>" + line + "</div>");
+            line = line.replace(/\[/g, "<span class='chord settingschord' style='top: -0.5em; line-height: 1; position: relative; margin: 0 2px 0 4px; color: red;'>").replace(/\]/g, "</span>");
+            line = ( "comment" === part.type || "choruscomment" === part.type ? "<div class=lyriccomment style='float: left; padding: 4px 8px; padding-bottom: 0.2em; background: #DDD; line-height: 1;'>" + line + "</div><div style='clear: both;'></div>" : line );
+            aResults.push("<div class=lyricline style='line-height: 1.8;'>" + line + "</div>");
         }
     }
 
@@ -406,214 +435,3 @@ function exportChordProPart(aParts, i) {
     return aResults.join("\n");
 }
 
-
-
-/*
-CVSNO
-const gSong = undefined;   // the song object is a global
-const giLine = 0;  // the current line number being parsed
-const gaLines = []; // the array of lines
-const gaDefaultSettings = {
-    "textfont": "Verdana, Arial, Helvetica, sans-serif",
-    "textsize": 14,
-    "textcolour": "black",
-    "chordfont": "Verdana, Arial, Helvetica, sans-serif",
-    "chordsize": 14,
-    "chordcolour": "red",
-    "tabfont": "Verdana, Arial, Helvetica, sans-serif",
-    "tabsize": 14,
-    "tabcolour": "black"
-};
-
-
-const importChordPro = ({text}) => {
-    const gSong = createSong();
-    gSong.chordprotext = text;
-
-    gaLines = text.split("\n");
-    giLine = 0;
-    while ( giLine < gaLines.length ) {
-        var line = gaLines[giLine].trim();
-        giLine++;
-        if ( isDirective(line) ) {
-            doDirective(line);
-        }
-        else if ( isComment(line) ) {
-            // do nothing
-        }
-        else if ( "" === line ) {
-            //console.log("DEBUG: blank line");
-        }
-        else {
-            // A line with no directive and it must NOT be within a directive block
-            // like chorus or verse. We assume this is lyrics so we create a verse.
-            giLine--; // include this line in the new verse
-            doBlock();
-        }
-    }
-
-    console.log(gSong);
-    return gSong;
-}
-
-
-const createSong = () => {
-    var song = {};
-
-    // placeholder for the parts of the song
-    song.parts = [];
-
-    // Initialize some settings:
-    for ( var name in gaDefaultSettings ) {
-        if ( gaDefaultSettings.hasOwnProperty(name) ) {
-            song[name] = gaDefaultSettings[name];
-        }
-    }
-
-    return song;
-};
-
-
-// For list of directives see:
-//   https://www.chordpro.org/chordpro/ChordPro-Directives.html
-const doDirective = ({line}) => {
-    var aMatches = line.match(/{([^:}]+)(.*)}/);
-    if ( ! aMatches ) {
-        console.log("Warning: Could not find directive in line: " + line);
-        return;
-    }
-
-    var directive = aMatches[1].toLowerCase().trim();
-    var parameters = "";
-    if ( aMatches[2] ) {
-        // These are the parameters to the directive.
-        parameters = aMatches[2].trim();
-        if ( ":" === parameters.charAt(0) ) {
-            // trim leading ":"
-            parameters = parameters.substring(1).trim();
-        }
-    }
-
-    // Translate song property aliases first.
-    switch ( directive ) {
-        case 't':
-            directive = 'title';
-            break;
-        case 'st':
-            directive = 'subtitle';
-            break;
-        // We accept the US spelling of "color" but export it as "colour".
-        case 'textcolor':
-            directive = "textcolour";
-            break;
-        case 'chordcolor':
-            directive = "chordcolour";
-            break;
-        case 'textcolor':
-            directive = "textcolour";
-            break;
-    }
-
-    switch ( directive ) {
-        // song properties
-        case 'title':
-        case 'subtitle':
-        case 'composer':
-        case 'artist':
-        case 'key':
-        case 'tempo':
-        case 'capo':
-        case 'textfont':
-        case 'textsize':
-        case 'textcolour':
-        case 'chordfont':
-        case 'chordsize':
-        case 'chordcolour':
-        case 'tabfont':
-        case 'tabsize':
-        case 'tabcolour':
-            gSong[directive] = parameters;
-            break;
-
-
-
-        // one-line but possibly recurring directives
-        case 'chorus':
-            // Technically the "chorus" directive just displays a comment that says "Chorus".
-            gSong.parts.push({'type': 'choruscomment', 'lines': ['Chorus']}); // TODO - this will NOT be styled like comments
-            break;
-        case 'c':
-        case 'comment':
-            gSong.parts.push({'type': 'comment', 'lines': [parameters]});
-            break;
-
-
-
-        // blocks of lyrics
-        case 'start_of_chorus':
-        case 'soc':
-            doBlock("chorus", ["end_of_chorus", "eoc"]);
-            break;
-        case 'start_of_verse':
-            doBlock("verse", "end_of_verse");
-            break;
-        case 'start_of_tab':
-        case 'sot':
-            doBlock("tab", ["end_of_tab", "eot"]);
-            break;
-        case 'end_of_chorus':
-        case 'eoc':
-        case 'end_of_verse':
-        case 'end_of_tab':
-        case 'eot':
-            console.log("ERROR: Should never reach this closing directive: " + directive);
-            break;
-
-
-        case 'ns':
-        case 'new_song':
-        case 'lyricist':
-        case 'copyright':
-        case 'album':
-        case 'year':
-        case 'time':
-        case 'duration':
-        case 'meta':
-        case 'comment_italic':
-        case 'ci':
-        case 'comment_box':
-        case 'cb':
-        case 'image':
-        case 'define':
-        case 'chord':
-
-        // no plan to support these
-        case 'start_of_grid':
-        case 'end_of_grid':
-        case 'new_page':
-        case 'np':
-        case 'new_physical_page':
-        case 'npp':
-        case 'column_break':
-        case 'cb':
-        case 'grid':
-        case 'g':
-        case 'no_grid':
-        case 'ng':
-        case 'titles':
-        case 'columns':
-        case 'col':
-
-        // Custom directives
-        case 'x_instrument': // guitar, ukulele, uke, bass, mandolin
-        case 'x_url':
-        case 'x_audio_url':
-        case 'x_youtube_url':
-            console.log("Warning: Directive \"" + directive + "\" is not supported currently.");
-            break;
-        default :
-            console.log("Warning: No handler for directive \"" + directive + "\".");
-    }
-};
-
-*/
