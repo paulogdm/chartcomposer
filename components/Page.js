ad03a70c962d56e56dc9f0f68eb57e550a3c3b70
Lindsey Simon <lindsey@quip.com>
2018-03-27 00:27:14 -0700

    trying the ole fashioned way, hardcoding

--- components/Page.js
+++ components/Page.js
@@ -1,3 +1,4 @@
+const { NOW_URL } = process.env;
 import Router from "next/router";
 
 import Meta from "./Meta";
@@ -14,10 +15,10 @@ export default ({ children }) => (
 const { Sender, Receiver } = createOauthFlow({
   authorizeUrl: "https://www.dropbox.com/oauth2/authorize",
   tokenUrl: "https://api.dropbox.com/oauth2/token",
-  clientId: process.env.CLIENT_ID,
-  clientSecret: process.env.CLIENT_SECRET,
-  redirectUri: process.env.DEV
-    ? "http://localhost:3000/authreceiver"
-    : "https://chartcomposer.com/authreceiver",
+  clientId: "mhwbhsacakthrrd",
+  clientSecret: "h9wig47grq00igb",
+  redirectUri: NOW_URL
+    ? "https://chartcomposer.com/authreceiver"
+    : "http://localhost:3000/authreceiver",
 });
 export { Sender, Receiver };
