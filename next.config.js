dc7a7763837e0002260a4f4271a6781493de7052
Lindsey Simon <lindsey@quip.com>
2018-03-27 00:01:32 -0700

    trying this for deploy

--- next.config.js
+++ next.config.js
@@ -1,4 +1,27 @@
 require('dotenv').config()
+
+const path = require('path')
+const Dotenv = require('dotenv-webpack')
+
+module.exports = {
+  webpack: (config) => {
+    config.plugins = config.plugins || []
+
+    config.plugins = [
+     ...config.plugins,
+
+     // Read the .env file
+     new Dotenv({
+          path: path.join(__dirname, '.env'),
+          systemvars: true
+        })
+    ]
+
+    return config
+  }
+}                                                
+
+/*require('dotenv').config()
 const webpack = require('webpack')
 module.exports = {
 	webpack: (config) => {
@@ -8,3 +31,4 @@ module.exports = {
 		return config
 	}
 }
+*/
