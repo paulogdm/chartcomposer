41b9cce83391ee50b13e5157260307933bb9eee8
Steve Souders <steve@speedcurve.com>
2018-04-26 11:23:59 -0700

    add editorClosed

--- components/SongView.jsx
+++ components/SongView.jsx
@@ -1,14 +1,21 @@
 import chordProParse, { parseChordProString } from "../utils/chordProParse.js";
 
-const SongView = ({ preferences, value }) => {
+const SongView = ({ preferences, value, toggleEditorClosed }) => {
+	  //toggleEditorClosed();
   //const obj = parseChordProString(value, preferences);
   return (
     <div>
       <style jsx>{`
         .lyricline {
-          color: green !important;
+          color: black !important;
         }
       `}</style>
+	  <div
+		 onClick={toggleEditorClosed}
+		 style={{ cursor: "pointer", padding: "0 10px 0 10px", "fontWeight": "bold", float: "right" }}
+		 >
+		&#x027F7;
+      </div>
       <div dangerouslySetInnerHTML={chordProParse(value, preferences)} />
     </div>
   );
