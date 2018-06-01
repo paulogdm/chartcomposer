4b93615d7ee6277401b88963cb52343b7f0acc41
Lindsey Simon <lindsey@quip.com>
2018-06-01 13:47:11 -0700

    Adds `now scale` to deploy script so that at least instance is always available

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 now -e DROPBOX_APP_KEY=@dropbox_app_key -e DROPBOX_APP_SECRET=@dropbox_app_secret -e DROPBOX_PUBLIC_TOKEN=@dropbox_public_token
 now alias
+now scale chartcomposer.com sfo 1
