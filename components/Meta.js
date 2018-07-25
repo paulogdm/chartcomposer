61b87b9fce6bf4f88264fca30546abbf09f2bafd
Lindsey Simon <lindsey@quip.com>
2018-07-25 07:36:49 -0700

    remove bootstrap print css overrides fixes #82

--- components/Meta.js
+++ components/Meta.js
@@ -2,12 +2,12 @@ import Head from "next/head";
 export default () => (
   <div>
     <Head>
-      <link rel="stylesheet" href="/static/bootstrap.min.css" />
       <meta
         name="viewport"
         content="width=device-width, initial-scale=1, user-scalable=no"
       />
       <meta charSet="utf-8" />
+      <link rel="stylesheet" href="/static/bootstrap.min.css" />
     </Head>
     <style jsx global>{`
       body {
